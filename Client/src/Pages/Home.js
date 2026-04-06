a3efb175f3c5f54f617a1a922e8a28d94c9fc47e
pratikshashetsandi87 <pratikshashetsandi90@gmail.com>
2026-04-06 19:29:22 +0530

    Update Home.js

--- Client/src/Pages/Home.js
+++ Client/src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import api from "../api.js";
+import api from "../api"; // ✅ FIX
 import { Checkbox, Radio, Input } from "antd";
 import { Prices } from "../Context/Prices";
 import Layout from "../Layout/Layout";
